3d9234533a80333518fea709c54189dbe4cbfe17
Webb Hinton <38958118+wyhinton@users.noreply.github.com>
2025-11-18 15:48:02 -0800

    AH-14 About page fixes

--- .svelte-kit/non-ambient.d.ts
+++ .svelte-kit/non-ambient.d.ts
@@ -27,17 +27,18 @@ export {};
 
 declare module "$app/types" {
 	export interface AppTypes {
-		RouteId(): "/" | "/about" | "/api" | "/api/image-proxy";
+		RouteId(): "/" | "/about" | "/api" | "/api/image-proxy" | "/[slug]";
 		RouteParams(): {
-			
+			"/[slug]": { slug: string }
 		};
 		LayoutParams(): {
-			"/": Record<string, never>;
+			"/": { slug?: string };
 			"/about": Record<string, never>;
 			"/api": Record<string, never>;
-			"/api/image-proxy": Record<string, never>
+			"/api/image-proxy": Record<string, never>;
+			"/[slug]": { slug: string }
 		};
-		Pathname(): "/" | "/about" | "/about/" | "/api" | "/api/" | "/api/image-proxy" | "/api/image-proxy/";
+		Pathname(): "/" | "/about" | "/about/" | "/api" | "/api/" | "/api/image-proxy" | "/api/image-proxy/" | `/${string}` & {} | `/${string}/` & {};
 		ResolvedPathname(): `${"" | `/${string}`}${ReturnType<AppTypes['Pathname']>}`;
 		Asset(): "/data/arrow_upward-24px.svg" | "/data/highlight_off-24px.svg" | "/data/introVideo.mp4" | "/data/menu-24px.svg" | "/data/OGImage.jpg" | "/favicon/android-chrome-192x192.png" | "/favicon/android-chrome-512x512.png" | "/favicon/apple-touch-icon.png" | "/favicon/browserconfig.xml" | "/favicon/favicon-16x16.png" | "/favicon/favicon-32x32.png" | "/favicon/favicon.ico" | "/favicon/mstile-144x144.png" | "/favicon/mstile-150x150.png" | "/favicon/mstile-310x150.png" | "/favicon/mstile-310x310.png" | "/favicon/mstile-70x70.png" | "/favicon/safari-pinned-tab.svg" | "/favicon/site.webmanifest" | "/images/close.png" | "/images/loading.gif" | "/images/next.png" | "/images/ooo.png" | "/images/prev.png" | string & {};
 	}
